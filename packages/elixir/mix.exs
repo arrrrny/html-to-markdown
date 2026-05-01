378e3754a18dafa20b212398fce848b9c6206e36
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2026-05-01 16:51:32 +0200

    chore(release): 3.4.0-rc.23
    Bump version across all manifests via `alef sync-versions --set`.
    
    Tests the new PIE binary publishing pipeline (alef 0.13.0 + the new
    package-php-pie shared action).
    
    Signed-off-by: Na'aman Hirschfeld <nhirschfeld@gmail.com>

--- packages/elixir/mix.exs
+++ packages/elixir/mix.exs
@@ -1,7 +1,7 @@
 defmodule Html_to_markdown.MixProject do
   use Mix.Project
 
-  @version "3.4.0-rc.22"
+  @version "3.4.0-rc.23"
   @source_url "https://github.com/kreuzberg-dev/html-to-markdown"
 
   def project do
--- packages/ruby/lib/html_to_markdown/version.rb
+++ packages/ruby/lib/html_to_markdown/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module HtmlToMarkdown
-  VERSION = '3.4.0.pre.rc.22'
+  VERSION = '3.4.0.pre.rc.23'
 end
